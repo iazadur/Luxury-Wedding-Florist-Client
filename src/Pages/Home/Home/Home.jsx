57193d4acc7c31bc8044dbd3d23482154cbf7b27
iazadur <iamazadur@gmail.com>
2021-11-09 23:47:41 +0600

    added login signin and checkout page in my client project

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Login from '../../Login/Login';
+import Register from '../../Register/Register';
 import Navigation from '../Navigation/Navigation';
 
 const Home = () => {
     return (
         <>
             <Navigation />
-            
+            <Login />
+            <Register />
+
         </>
     );
 };
